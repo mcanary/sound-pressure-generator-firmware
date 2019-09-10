221b2aa4821b033d5ca2a336762554deaf220648
mcanary <canarymc1@gmail.com>
2019-09-09 20:35:36 -0700

    #1 Added GPIO HAL

---
+++ hal/inc/gpio.h
@@ -0,0 +1,73 @@
+
+
+/* GPIO register offsets */
+#define GPIO_MODER      0x00
+#define GPIO_OTYPER     0x04
+#define GPIO_OSPEEDR    0x08
+#define GPIO_PUPDR      0x0c
+#define GPIO_IDR        0x10
+#define GPIO_ODR        0x14
+#define GPIO_AFRL       0x20
+#define GPIO_AFRH       0x24
+
+
+/* Port and pin enumerations */
+#define GPIO_PORTA      0x40020000
+#define GPIO_PORTB      0x40020400
+#define GPIO_PORTC      0x40020800
+#define GPIO_PORTD      0x40020c00
+#define GPIO_PORTE      0x40021000
+#define GPIO_PORTH      0x40021c00
+
+#define GPIO_PIN_0      0x0
+#define GPIO_PIN_1      0x1
+#define GPIO_PIN_2      0x2
+#define GPIO_PIN_3      0x3
+#define GPIO_PIN_4      0x4
+#define GPIO_PIN_5      0x5
+#define GPIO_PIN_6      0x6
+#define GPIO_PIN_7      0x7
+#define GPIO_PIN_8      0x8
+#define GPIO_PIN_9      0x9
+#define GPIO_PIN_10     0xa
+#define GPIO_PIN_11     0xb
+#define GPIO_PIN_12     0xc
+#define GPIO_PIN_13     0xd
+#define GPIO_PIN_14     0xe
+#define GPIO_PIN_15     0xf
+
+
+/* GPIO State Enumerations */
+#define GPIO_MODE_INPUT                 0x0
+#define GPIO_MODE_GPO                   0x1
+#define GPIO_MODE_ALTERNATE_FUNCTION    0x2
+#define GPIO_MODE_ANALOG                0x3
+
+#define GPIO_OUTPUT_TYPE_PUSH_PULL      0x0
+#define GPIO_OUTPUT_TYPE_OPEN_DRAIN     0x1
+
+#define GPIO_OUTPUT_SPEED_LOW           0x0
+#define GPIO_OUTPUT_SPEED_MEDIUM        0x1
+#define GPIO_OUTPUT_SPEED_FAST          0x2
+#define GPIO_OUTPUT_SPEED_HIGH          0x3
+
+#define GPIO_PUPD_NONE                  0x0
+#define GPIO_PUPD_PULL_UP               0x1
+#define GPIO_PUPD_PULL_DOWN             0x2
+
+
+/* Typedefs */
+typedef struct gpio_init_struct {
+    uint8_t mode;
+    uint8_t output_type;
+    uint8_t output_speed;
+    uint8_t pull_up_pull_down;
+}gpio_init_struct;
+
+
+/* Function prototypes */
+void gpio_init(uint32_t port, uint8_t pin, gpio_init_struct * init_struct);
+bool gpio_read(uint32_t port, uint8_t pin);
+void gpio_reset(uint32_t port, uint8_t pin);
+void gpio_set(uint32_t port, uint8_t pin);
+
---
+++ hal/src/gpio.c
@@ -0,0 +1,51 @@
+
+
+#include "gpio.h"
+
+
+void gpio_init(uint32_t port, uint8_t pin, gpio_init_struct * init_struct) {
+
+    volatile uint32_t * mode_register = (uint32_t *) port + GPIO_MODER;
+    volatile uint32_t * otype_register = (uint32_t *) port + GPIO_OTYPER;
+    volatile uint32_t * ospeed_register = (uint32_t *) port + GPIO_OSPEEDR;
+    volatile uint32_t * pupd_register = (uint32_t *) port + GPIO_PUPDR;
+
+    uint32_t mode = * mode_register & ~(0x3 << (2 * pin));
+    uint32_t otype = * otype_register & ~(0x1 << pin);
+    uint32_t ospeed = * ospeed_register & ~(0x3 << (2 * pin));
+    uint32_t pupd = * pupd_register & ~(0x3 << (2 * pin));
+
+    mode = mode | (init_struct->mode << (0x2 * pin));
+    otype = otype | (init_struct->output_type << pin);
+    ospeed = ospeed | (init_struct->output_speed << (0x2 * pin));
+    pupd = pupd | (init_struct->pull_up_pull_down << (0x2 * pin));
+
+    * mode_register = mode;
+    * otype_register = otype;
+    * ospeed_register = ospeed;
+    * pupd_register = pupd;
+
+}
+
+bool gpio_read(uint32_t port, uint8_t pin) {
+
+    volatile uint32_t * id_register = (uint32_t *) port + GPIO_IDR;
+    return (* id_register & (0x1 << pin)) >> pin;
+    
+}
+
+void gpio_reset(uint32_t port, uint8_t pin) {
+
+    volatile uint32_t * od_register = (uint32_t *) port + GPIO_ODR;
+    * od_register = * od_register & ~(0x1 << pin);
+
+}
+
+void gpio_set(uint32_t port, uint8_t pin) {
+
+    volatile uint32_t * od_register = (uint32_t *) port + GPIO_ODR;
+    * od_register = * od_register | (0x1 << pin);
+
+}
+
+
